c41cd5911bece4f9204ba988ebebd4cb77c8f4e4
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2023-04-13 18:10:22 -0600

    getEnvironments actualizado

--- src/helpers/getEnvironments.js
+++ src/helpers/getEnvironments.js
@@ -5,10 +5,8 @@ export const getEnvironments = () => {
 
 
     if (typeof process !== "undefined") {
-      process.env
       return { ...process.env}
     } else {
-      import.meta.env
       return { ...import.meta.env}
     }
   }
